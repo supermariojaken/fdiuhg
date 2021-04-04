d5df44b59b3a440a89cb86e8954306d828ee5c36
supermariojaken <75708558+supermariojaken@users.noreply.github.com>
2021-04-03 23:02:01 -0400

    Update page.js

--- static/page.js
+++ static/page.js
@@ -99,7 +99,7 @@ module.exports = function (req, res, url) {
 			break;
 		}
 
-		case "/go_full": {
+		case "/videomaker/full": {
 			let presave =
 				query.movieId && query.movieId.startsWith("m")
 					? query.movieId
